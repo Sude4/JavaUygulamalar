cf3bc4dd074abb3a14d8583d4cf3726c6caf43a3
Meryem Sude ÇETİN <56775168+Sude4@users.noreply.github.com>
2021-04-26 16:02:36 +0300

    Add files via upload

---
+++ javaUygulamalar/sesliHarfler/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+	char harf='E';
+
+
+	switch(harf){
+        case 'A':
+        case'I':
+        case'0':
+        case'U':
+                System.out.println("Kalın sesli harf");
+                break;
+        default:
+            System.out.println("İnce sesli harf");
+    }
+    }
+}
